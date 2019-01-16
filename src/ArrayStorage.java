b3caf1c0ba83dbc2f53b345418dc2d74690b8ebc
Marianych <maryanich.sergey@gmail.com>
2019-01-16 21:36:22 +0200

    без сортировки

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,18 +3,40 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int id = -1;
+    int i=0;
 
     void clear() {
+        for (Resume e : storage) if (e != null) e = null;
     }
 
     void save(Resume r) {
+        for (Resume e : storage){
+            if (e == null) {
+                e = r;
+                break;
+            }
+            }
     }
 
     Resume get(String uuid) {
-        return null;
+        for (int i = 0; i < storage.length; i++) {
+
+            if (storage[i].uuid == uuid) {
+                id = i;
+                break;
+
+            }
+        }
+        return storage[id];
     }
 
     void delete(String uuid) {
+        for (Resume e : storage) {
+            if (e.uuid == uuid) e = null;
+
+        }
+
     }
 
     /**
