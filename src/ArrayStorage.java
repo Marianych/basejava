2479ffec37b303bf8464ff43d5255793a39b50c6
Marianych <maryanich.sergey@gmail.com>
2019-01-19 21:06:32 +0200

    fix code style & code optimization

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,66 +1,64 @@
-/**
- * Array based storage for Resumes
+/*
+  Array based storage for Resumes
  */
+
+
+import static java.lang.System.arraycopy;
+
 public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
+    private int s = 0;
 
     void clear() {
-        for (int i = 0; i < storage.length; i++)
+        for (int i = 0; i < s; i++)
             storage[i] = null;
+        s = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++)
-            if (storage[i] == null) {
-                storage[i] = r;
-                break;
-            }
+        s++;
+        for (int i = 0; i < s; i++)
+            if (storage[i] == null) storage[i] = r;
     }
 
     Resume get(String uuid) {
-        int id = -1;
-        Resume buf = null;
-        for (int i = 0; i < storage.length; i++) {
-            if ((storage[i] != null) && (storage[i].uuid.equals(uuid))) {
-                id = i;
-                break;
-            }
-        }
-        if (id >= 0) {
-            buf = storage[id];
-        }
-        return buf;
+        Resume found = null;
+        int id = indexOf(uuid);
+        if (id >= 0) found = storage[id];
+        return found;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            if ((storage[i] != null) && (storage[i].uuid.equals(uuid)))
-                storage[i] = null;
-        }
+        int i = indexOf(uuid);
+        if (i >= 0) {
+            arraycopy(storage, i + 1, storage, i, s - i - 1);
+            s--;
+        } else
+            System.out.println("Нельзя удалить то, чего нет");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int i = 0;
         Resume[] newStorage = new Resume[this.size()];
-        for (Resume e : storage) {
-            if (e != null) {
-                newStorage[i] = e;
-                i++;
-            }
-        }
+        arraycopy(storage, 0, newStorage, 0, s);
         return newStorage;
     }
 
     int size() {
-        int s = 0;
-        for (Resume e : storage) {
-            if (e != null) {
-                s++;
+        return s;
+    }
+
+    private int indexOf(String uuid) {
+        int index = -1;
+        for (int i = 0; i < s; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                index = i;
+                break;
             }
         }
-        return s;
+        return index;
+
     }
 }
