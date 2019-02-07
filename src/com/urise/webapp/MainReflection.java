a6fa0e0ca2411171d759c3e00aebc63042f9b053
Marianych <maryanich.sergey@gmail.com>
2019-02-07 17:02:03 +0200

    meeting with reflection

---
+++ src/com/urise/webapp/MainReflection.java
@@ -0,0 +1,18 @@
+package com.urise.webapp;
+
+import com.urise.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        System.out.println(field.getName());
+        field.setAccessible(true);
+        System.out.println(field.get(r));
+        field.set(r, "new uuid");
+        //TODO invoke r.toString via reflection
+        System.out.println(r);
+    }
+}
